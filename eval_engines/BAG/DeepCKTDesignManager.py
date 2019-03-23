0bf8ef2a5295c6bcca375cd8d6b09949681b16ec
Kourosh Hakhamaneshi <kourosh_hakhamaneshi@bwrcrdsl-6.eecs.berkeley.edu>
2019-03-22 17:22:36 -0700

    Modified DesignManager for more compatibility with deep_ckt needs

--- eval_engines/BAG/DeepCKTDesignManager.py
+++ eval_engines/BAG/DeepCKTDesignManager.py
@@ -1,13 +1,15 @@
 from bag.simulation.core import DesignManager
-from bag.core import BagProject
-from bag.io import read_yaml
 from copy import deepcopy
 
-from bag.layout import RoutingGrid, TemplateDB
-from bag import float_to_si_string
-from bag.io import read_yaml, open_file, load_sim_results, save_sim_results, load_sim_file
+from typing import Dict, Any, Tuple, Sequence
+
+import importlib
+
+from bag.io import read_yaml
+from bag.layout import TemplateDB
 from bag.concurrent.core import batch_async_task
-from bag import BagProject
+import pdb
+import pprint
 
 class DeepCKTDesignManager(DesignManager):
     # Modifications:
@@ -19,6 +21,7 @@ class DeepCKTDesignManager(DesignManager):
     # 1. It should associate schematic and layout together always
     # 2. Should not replace layout and dump the schematic in somewhere else
     # 3. make gen_wrapper=True if wrapper is given, if not make it false
+    # 4. make it such that the circuit can be broken down to sub circuits and individually perform simulation on it
 
     def __init__(self, *args, **kwargs):
         self._temp_db = None
@@ -70,7 +73,8 @@ class DeepCKTDesignManager(DesignManager):
         start = time.time()
         if create_layout:
             print('creating all layouts.')
-            sch_params_list = self.create_dut_layouts(lay_params_list, dsn_name_list, temp_db)
+            sch_params_list = self.create_dut_layouts(lay_params_list, dsn_name_list, temp_db, throw_errors=False,
+                                                      log_file='./layout_errors.txt')
         else:
             print('schematic simulation, skipping layouts.')
             sch_params_list = [self.get_schematic_params(combo_list)
@@ -78,7 +82,7 @@ class DeepCKTDesignManager(DesignManager):
         print("Layout Creation time:{}".format(time.time()-start))
         start = time.time()
         print('creating all schematics.')
-        self.create_dut_schematics(sch_params_list, dsn_name_list, gen_wrappers=True)
+        self.create_dut_schematics(sch_params_list, dsn_name_list, gen_wrappers=True, throw_errors=False)
 
         print("Schematic Creation time:{}".format(time.time()-start))
         print('design generation done.')
@@ -120,3 +124,75 @@ class DeepCKTDesignManager(DesignManager):
         #         if isinstance(val, Exception):
         #             raise val
         return results
+
+    def create_dut_schematics(self, sch_params_list, cell_name_list, gen_wrappers=True, throw_errors=True):
+        # type: (Sequence[Dict[str, Any]], Sequence[str], bool) -> None
+        dut_lib = self.specs['dut_lib']
+        dut_cell = self.specs['dut_cell']
+        impl_lib = self.specs['impl_lib']
+        wrapper_list = self.specs['dut_wrappers']
+
+        inst_list, name_list = [], []
+        for sch_params, cur_name in zip(sch_params_list, cell_name_list):
+            try:
+                dsn = self.prj.create_design_module(dut_lib, dut_cell)
+                dsn.design(**sch_params)
+                inst_list.append(dsn)
+                name_list.append(cur_name)
+                if gen_wrappers:
+                    for wrapper_config in wrapper_list:
+                        wrapper_name = wrapper_config['name']
+                        wrapper_lib = wrapper_config['lib']
+                        wrapper_cell = wrapper_config['cell']
+                        wrapper_params = wrapper_config['params'].copy()
+                        wrapper_params['dut_lib'] = impl_lib
+                        wrapper_params['dut_cell'] = cur_name
+                        dsn = self.prj.create_design_module(wrapper_lib, wrapper_cell)
+                        dsn.design(**wrapper_params)
+                        inst_list.append(dsn)
+                        name_list.append(self.get_wrapper_name(cur_name, wrapper_name))
+            except Exception as e:
+                if throw_errors:
+                    raise e
+
+        if inst_list:
+            self.prj.batch_schematic(impl_lib, inst_list, name_list=name_list)
+
+    def create_dut_layouts(self, lay_params_list, cell_name_list, temp_db, throw_errors=True, log_file=None):
+        # type: (Sequence[Dict[str, Any]], Sequence[str], TemplateDB) -> Sequence[Dict[str, Any]]
+        """Create multiple layouts"""
+        if self.prj is None:
+            raise ValueError('BagProject instance is not given.')
+
+        assert len(lay_params_list) == len(cell_name_list), 'lay_param_list and cell_name_list length mismatch'
+
+        cls_package = self.specs['layout_package']
+        cls_name = self.specs['layout_class']
+
+        lay_module = importlib.import_module(cls_package)
+        temp_cls = getattr(lay_module, cls_name)
+
+        temp_list, sch_params_list = [], []
+        removing_indices = []
+        if log_file:
+            pprint.pprint("******** layout log file:", stream=open(log_file, 'w'))
+        for i, (lay_params, cell_name) in enumerate(zip(lay_params_list, cell_name_list)):
+            try:
+                template = temp_db.new_template(params=lay_params, temp_cls=temp_cls)
+                temp_list.append(template)
+                sch_params_list.append(template.sch_params)
+            except Exception as e:
+                if throw_errors:
+                    raise e
+                else:
+                    if log_file:
+                        pprint.pprint("-"*50, stream=open(log_file, 'a'))
+                        pprint.pprint(e, stream=open(log_file, 'a'))
+                        pprint.pprint(lay_params, stream=open(log_file, 'a'))
+                    removing_indices.append(i)
+
+        for i in sorted(removing_indices, reverse=True):
+            del cell_name_list[i]
+
+        temp_db.batch_layout(self.prj, temp_list, cell_name_list)
+        return sch_params_list
