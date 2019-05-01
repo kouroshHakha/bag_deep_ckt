0adb6546add64f0fdbce46d26a43c2f2742b0d59
Kourosh Hakhamaneshi <kourosh_hakhamaneshi@bwrcrdsl-6.eecs.berkeley.edu>
2019-04-30 19:41:59 -0700

    parsing done

--- eval_engines/spectre/core.py
+++ eval_engines/spectre/core.py
@@ -1,15 +1,19 @@
 import re
-import libpsf
 import copy
 import os
-import jinja2
+from jinja2 import Environment, FileSystemLoader
 import os
 import subprocess
 from multiprocessing.dummy import Pool as ThreadPool
 import yaml
 import importlib
+import random
+import numpy as np
+from eval_engines.util.core import IDEncoder, Design
+from eval_engines.spectre.parser import SpectreParser
 
-debug = False
+
+debug = True
 
 def get_config_info():
     # TODO
@@ -20,9 +24,11 @@ def get_config_info():
     else:
         config_info['BASE_TMP_DIR'] = base_tmp_dir
 
+    return config_info
+
 class SpectreWrapper(object):
 
-    def __init__(self, netlist_loc, post_process_fn=None):
+    def __init__(self, tb_dict):
         """
         This Wrapper handles one netlist at a time, meaning that if there are multiple test benches
         for characterizations multiple instances of this class needs to be created
@@ -34,22 +40,28 @@ class SpectreWrapper(object):
         # config_info also contains BASE_TMP_DIR (location for storing simulation netlist/results)
         # implement get_config_info() later
 
+        netlist_loc = tb_dict['netlist_template']
+        if not os.path.isabs(netlist_loc):
+            netlist_loc = os.path.abspath(netlist_loc)
+        pp_module = importlib.import_module(tb_dict['tb_module'])
+        pp_class = getattr(pp_module, tb_dict['tb_class'])
+        self.post_process = getattr(pp_class, tb_dict['post_process_function'])
+        self.tb_params = tb_dict['tb_params']
+
         self.config_info = get_config_info()
 
         self.root_dir = self.config_info['BASE_TMP_DIR']
         self.num_process = self.config_info.get('NUM_PROCESS', 1)
 
-        _, dsg_netlist_fname = os.path.split(netlist_loc)
-        self.base_design_name = os.path.splitext(dsg_netlist_fname)[0]
+        _, dsn_netlist_fname = os.path.split(netlist_loc)
+        self.base_design_name = os.path.splitext(dsn_netlist_fname)[0]
         self.gen_dir = os.path.join(self.root_dir, "designs_" + self.base_design_name)
 
         os.makedirs(self.gen_dir, exist_ok=True)
 
-        self.post_process = post_process_fn
-        raw_file = open(netlist_loc, 'r')
-        self.tmp_lines = raw_file.readlines()
-        raw_file.close()
-
+        file_loader = FileSystemLoader(os.path.dirname(netlist_loc))
+        self.jinja_env = Environment(loader=file_loader)
+        self.template = self.jinja_env.get_template(dsn_netlist_fname)
 
     def _get_design_name(self, state):
         """
@@ -63,55 +75,28 @@ class SpectreWrapper(object):
         return fname
 
     def _create_design(self, state, new_fname):
-        # TODO: Replace these functions with jinja2
+        output = self.template.render(**state)
         design_folder = os.path.join(self.gen_dir, new_fname)
         os.makedirs(design_folder, exist_ok=True)
-
-        fpath = os.path.join(design_folder, new_fname + '.cir')
-
-        lines = copy.deepcopy(self.tmp_lines)
-        for line_num, line in enumerate(lines):
-            if '.include' in line:
-                regex = re.compile("\.include\s*\"(.*?)\"")
-                found = regex.search(line)
-                if found:
-                    # current_fpath = os.path.realpath(__file__)
-                    # parent_path = os.path.abspath(os.path.join(current_fpath, os.pardir))
-                    # parent_path = os.path.abspath(os.path.join(parent_path, os.pardir))
-                    # path_to_model = os.path.join(parent_path, 'spice_models/45nm_bulk.txt')
-                    # lines[line_num] = lines[line_num].replace(found.group(1), path_to_model)
-                    pass # do not change the model path
-            if '.param' in line:
-                for key, value in state.items():
-                    regex = re.compile("%s=(\S+)" % (key))
-                    found = regex.search(line)
-                    if found:
-                        new_replacement = "%s=%s" % (key, str(value))
-                        lines[line_num] = lines[line_num].replace(found.group(0), new_replacement)
-            if 'wrdata' in line:
-                regex = re.compile("wrdata\s*(\w+\.\w+)\s*")
-                found = regex.search(line)
-                if found:
-                    replacement = os.path.join(design_folder, found.group(1))
-                    lines[line_num] = lines[line_num].replace(found.group(1), replacement)
-
+        fpath = os.path.join(design_folder, new_fname + '.scs')
         with open(fpath, 'w') as f:
-            f.writelines(lines)
+            f.write(output)
             f.close()
         return design_folder, fpath
 
     def _simulate(self, fpath):
-        # TODO run spectre
-        command = ['spectre', '%s'%fpath, '>/dev/null 2>&1']
-        exit_code = subprocess.call(command, cwd=os.path.dirname(fpath))
+        command = ['spectre', '%s'%fpath, '-format', 'psfbin' ,'> /dev/null 2>&1']
+        log_file = os.path.join(os.path.dirname(fpath), 'log.txt')
+        err_file = os.path.join(os.path.dirname(fpath), 'err_log.txt')
+        exit_code = subprocess.call(command, cwd=os.path.dirname(fpath),
+                                    stdout=open(log_file, 'w'), stderr=open(err_file, 'w'))
         info = 0
         if debug:
             print(command)
             print(fpath)
-
         if (exit_code % 256):
-            # raise RuntimeError('program {} failed!'.format(command))
             info = 1 # this means an error has occurred
+
         return info
 
 
@@ -129,14 +114,16 @@ class SpectreWrapper(object):
         info = self._simulate(fpath)
         results = self._parse_result(design_folder)
         if self.post_process:
-            specs = self.post_process(results)
+            specs = self.post_process(results, self.tb_params)
             return state, specs, info
         specs = results
         return state, specs, info
 
     def _parse_result(self, design_folder):
-        # TODO: kourosh
-        pass
+        _, folder_name = os.path.split(design_folder)
+        raw_folder = os.path.join(design_folder, '{}.raw'.format(folder_name))
+        res = SpectreParser.parse(raw_folder)
+        return res
 
     def run(self, states, design_names=None, verbose=False):
         # TODO: Use asyncio to instantiate multiple jobs for running parallel sims
@@ -156,28 +143,36 @@ class SpectreWrapper(object):
 
 
 class EvaluationEngine(object):
+
     def __init__(self, yaml_fname):
 
         self.design_specs_fname = yaml_fname
         with open(yaml_fname, 'r') as f:
             self.ver_specs = yaml.load(f)
 
-        meas_module = importlib.import_module(self.ver_specs['measurement']['meas_module'])
-        meas_cls = getattr(meas_module, self.ver_specs['measurement']['meas_class'])
+        self.spec_range = self.ver_specs['spec_range']
+        # params are interfaced using the index instead of the actual value
+        params = self.ver_specs['params']
 
-        self.measMan = meas_cls(yaml_fname)
-        self.params = self.measMan.params
-        self.spec_range = self.measMan.spec_range
-        self.params_vec = self.measMan.params_vec
+        self.params_vec = {}
+        self.search_space_size = 1
+        for key, value in params.items():
+            self.params_vec[key] = np.arange(value[0], value[1], value[2]).tolist()
+            self.search_space_size = self.search_space_size * len(self.params_vec[key])
+
         # minimum and maximum of each parameter
         # params_vec contains the acutal numbers but min and max should be the indices
         self.params_min = [0]*len(self.params_vec)
-
         self.params_max = []
         for val in self.params_vec.values():
             self.params_max.append(len(val)-1)
 
         self.id_encoder = IDEncoder(self.params_vec)
+        self.measurement_specs = self.ver_specs['measurement']
+        tbs = self.measurement_specs['testbenches']
+        self.netlist_module_dict = {}
+        for tb_kw, tb_val in tbs.items():
+            self.netlist_module_dict[tb_kw] = SpectreWrapper(tb_val)
 
 
     @property
@@ -201,8 +196,8 @@ class EvaluationEngine(object):
             design = Design(self.spec_range, self.id_encoder, list(design.values()))
             if design in tried_designs:
                 if (useless_iter_count > n * 5):
-                    raise ValueError("Random selection of a fraction of search space did not result in {}"
-                                     "number of valid designs".format(n))
+                    raise ValueError("Random selection of a fraction of search space did not "
+                                     "result in {} number of valid designs".format(n))
                 useless_iter_count += 1
                 continue
             design_result = self.evaluate([design])[0]
@@ -218,6 +213,7 @@ class EvaluationEngine(object):
 
     def evaluate(self, design_list):
         """
+        serial implementation of evaluate (not parallel)
         :param design_list:
         :return: a list of processed_results that the algorithm cares about, keywords should include
         cost and spec keywords with one scalar number as the value for each
@@ -225,7 +221,7 @@ class EvaluationEngine(object):
         results = []
         for design in design_list:
             try:
-                result = self.measMan.evaluate(design)
+                result = self._evaluate(design)
                 result['valid'] = True
             except Exception as e:
                 result = {'valid': False}
@@ -233,9 +229,21 @@ class EvaluationEngine(object):
             results.append(result)
         return results
 
-    def compute_penalty(self, spec_nums, spec_kwrd):
-        return self.measMan.compute_penalty(spec_nums, spec_kwrd)
+    def _evaluate(self, design):
+        state_dict = dict()
+        for i, key in enumerate(self.params_vec.keys()):
+            state_dict[key] = self.params_vec[key][design[i]]
+        state = [state_dict]
+        dsn_names = [design.id]
+        results = {}
+        for netlist_name, netlist_module in self.netlist_module_dict.items():
+            results[netlist_name] = netlist_module.run(state, dsn_names)
 
+        import pdb
+        pdb.set_trace()
+        specs_dict = self.get_specs(results, self.measurement_specs['meas_params'])
+        specs_dict['cost'] = self.cost_fun(specs_dict)
+        return specs_dict
 
     def find_worst(self, spec_nums, spec_kwrd, ret_penalty=False):
         if not hasattr(spec_nums, '__iter__'):
@@ -248,5 +256,31 @@ class EvaluationEngine(object):
             return spec_nums[worst_idx], worst_penalty
         else:
             return spec_nums[worst_idx]
+
+    def cost_fun(self, specs_dict):
+        """
+        :param design: a list containing relative indices according to yaml file
+        :param verbose:
+        :return:
+        """
+        cost = 0
+        for spec in self.spec_range.keys():
+            penalty = self.compute_penalty(specs_dict[spec], spec)[0]
+            cost += penalty
+
+        return cost
+
+    def get_specs(self, results, params):
+        """
+        converts results from different tbs to a single dictionary summarizing everything
+        :param results:
+        :return:
+        """
+        raise NotImplementedError
+
+    def compute_penalty(self, spec_nums, spec_kwrd):
+        # use self.spec_range[spec_kwrd] to get the min, max, and weight
+        raise NotImplementedError
+
 
 
