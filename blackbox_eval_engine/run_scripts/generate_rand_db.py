3d57ec251fb44358ebb57f29054f82b7099eab6a
kourosh <kourosh_hakhamaneshi@berkeley.edu>
2019-10-13 11:23:43 -0700

    renamed

---
+++ blackbox_eval_engine/run_scripts/generate_rand_db.py
@@ -0,0 +1,56 @@
+import argparse
+from argparse import Namespace
+from pathlib import Path
+from bb_eval_engine.util.importlib import import_cls
+from bb_eval_engine.base import EvaluationEngineBase
+
+import yaml
+import pickle
+import time
+
+
+def parse_args() -> Namespace:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('specs_fname', help='specs yaml file')
+    parser.add_argument('db_path', help='location to store the db file')
+    parser.add_argument('-n', '--number', dest='number', type=int, default=1,
+                        help='number of individuals in the database')
+    parser.add_argument('-s', '--seed', default=None, type=int,
+                        help='the seed used for generating the data base')
+    parser.add_argument('-v', '--verbose', default=False, action='store_true',
+                        help='True to make the underlying processes verbose')
+    parser.add_argument('-p', '--processes', default=False, action='store_true',
+                        help='True to make multi-processes enable, default is multi-thread')
+    args = parser.parse_args()
+    return args
+
+
+def run_main(args: Namespace):
+
+    with open(args.specs_fname, 'r') as f:
+        specs = yaml.load(f, Loader=yaml.Loader)
+
+    kwargs = dict(
+        verbose=args.verbose,
+        processes=args.processes,
+    )
+
+    eval_engine_str = specs['eval_engine_cls']
+    eval_engine_params = specs['eval_engine_params']
+    eval_engine_cls = import_cls(eval_engine_str)
+    eval_engine: EvaluationEngineBase = eval_engine_cls(specs=eval_engine_params, **kwargs)
+    start = time.time()
+    designs = eval_engine.generate_rand_designs(n=args.number, evaluate=True, seed=args.seed)
+
+    db_path = Path(args.db_path)
+    db_path.parent.mkdir(exist_ok=True, parents=True)
+    with open(db_path, 'wb') as f:
+        pickle.dump(designs, f, pickle.HIGHEST_PROTOCOL)
+
+    print(f'data base stored in {str(db_path)}')
+    print(f'random generation of {args.number} samples took {time.time() - start:.6} seconds')
+
+
+if __name__ == '__main__':
+    args = parse_args()
+    run_main(args)
---
+++ blackbox_eval_engine/src/bb_eval_engine/circuits/spectre/script_test/cs_meas_man.py
@@ -0,0 +1,94 @@
+from typing import Dict, Any
+from bb_eval_engine.src.eval_engines.circuits import EvaluationEngine, SubEngine
+import scipy.interpolate as interp
+import scipy.optimize as sciopt
+import random
+import numpy as np
+import pdb
+
+
+class CSMeasMan(EvaluationEngine):
+
+    def __init__(self, yaml_fname):
+        EvaluationEngine.__init__(self, yaml_fname)
+
+
+    def get_specs(self, results_dict, params):
+        _, specs_dict, _ = results_dict['ac_dc']
+        return specs_dict
+
+    def compute_penalty(self, spec_nums, spec_kwrd):
+        if not hasattr(spec_nums, '__iter__'):
+            spec_nums = [spec_nums]
+        penalties = []
+        for spec_num in spec_nums:
+            penalty = 0
+            spec_min, spec_max, w = self.spec_range[spec_kwrd]
+            if spec_max is not None:
+                if spec_num > spec_max:
+                    penalty += w * abs(spec_num - spec_max) / abs(spec_num)
+            if spec_min is not None:
+                if spec_num < spec_min:
+                    penalty += w * abs(spec_num - spec_min) / abs(spec_min)
+            penalties.append(penalty)
+        return penalties
+
+
+
+class ACTB(SubEngine):
+
+    @classmethod
+    def process(cls, results: Dict[str, Any], params: Dict[str, Any]) -> Dict[str, Any]:
+        ac_result = results['ac']
+        dc_results = results['dcOp']
+
+        vout = ac_result['out']
+        freq = ac_result['sweep_values']
+
+        gain = cls.find_dc_gain(vout)
+        bw = cls.find_bw(vout, freq)
+
+        results = dict(
+            gain=gain,
+            bw=bw
+        )
+        return results
+
+    @classmethod
+    def find_dc_gain (cls, vout):
+        return np.abs(vout)[0]
+
+    @classmethod
+    def find_bw(cls, vout, freq):
+        gain = np.abs(vout)
+        gain_3dB = gain[0] / np.sqrt(2)
+        return cls._get_best_crossing(freq, gain, gain_3dB)
+
+    @classmethod
+    def _get_best_crossing(cls, xvec, yvec, val):
+        interp_fun = interp.InterpolatedUnivariateSpline(xvec, yvec)
+
+        def fzero(x):
+            return interp_fun(x) - val
+
+        xstart, xstop = xvec[0], xvec[-1]
+        try:
+            return sciopt.brentq(fzero, xstart, xstop)
+        except ValueError:
+            # avoid no solution
+            if abs(fzero(xstart)) < abs(fzero(xstop)):
+                return xstart
+            return xstop
+
+def set_seed(seed):
+    random.seed(seed)
+    np.random.seed(seed)
+
+if __name__ == '__main__':
+
+    set_seed(10)
+    yname = 'bag_deep_ckt/blackbox_eval_engine/spectre/specs_test/common_source.yaml'
+    eval_core = CSMeasMan(yname)
+
+    designs = eval_core.generate_data_set(n=100, debug=False)
+    pdb.set_trace()
